1aee9e76657add52d96ec33275d202ad89a16af8
youngjn <gkgkgk98@gmail.com>
2021-10-05 04:11:39 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,955 @@
+$(function() {
+		  
+
+		});
+
+
+function twoth() {
+
+	var current = $(".number").text();
+	var currentInt = parseInt(current);
+
+
+	countUp();
+
+	function countUp() {
+		var intervv=setInterval(add, 1);
+
+		function add() {
+			if (currentInt > 232500000) {
+				currentInt-=10000;
+				if(currentInt < 232500000) {
+					clearInterval(intervv);
+				} else {
+					$(".number").text(currentInt).toLocaleString('en-US');
+				}
+			} else if(currentInt == 232500000) {
+				clearInterval(intervv);
+			} else {
+				currentInt+=10000;
+				if (currentInt > 232500000) {
+				        clearInterval(intervv);
+				      } else {
+				        $(".number").text(currentInt).toLocaleString('en-US');
+				      }
+			}
+
+			
+		}
+	}
+	
+
+	current = $(".number").text();
+	currentInt = parseInt(current);
+}
+
+
+
+function twothone() {
+
+	var current = $(".number").text();
+	var currentInt = parseInt(current);
+
+
+	countUp();
+
+	function countUp() {
+		var intervv=setInterval(add, 1);
+
+		function add() {
+			if (currentInt > 235690000) {
+				currentInt-=10000;
+				if(currentInt < 235690000) {
+					clearInterval(intervv);
+				} else {
+					$(".number").text(currentInt).toLocaleString('en-US');
+				}
+			} else if(currentInt == 235690000) {
+				clearInterval(intervv);
+			} else {
+				currentInt+=10000;
+				if (currentInt > 235690000) {
+				        clearInterval(intervv);
+				      } else {
+				        $(".number").text(currentInt).toLocaleString('en-US');
+				      }
+			}
+
+			
+		}
+	}
+	
+
+	current = $(".number").text();
+	currentInt = parseInt(current);
+}
+
+function twothtwo() {
+
+	var current = $(".number").text();
+	var currentInt = parseInt(current);
+
+
+	countUp();
+
+	function countUp() {
+		var intervv=setInterval(add, 1);
+
+		function add() {
+			if (currentInt > 242660000) {
+				currentInt-=10000;
+				if(currentInt < 242660000) {
+					clearInterval(intervv);
+				} else {
+					$(".number").text(currentInt).toLocaleString('en-US');
+				}
+			} else if(currentInt == 242660000) {
+				clearInterval(intervv);
+			} else {
+				currentInt+=10000;
+				if (currentInt > 242660000) {
+				        clearInterval(intervv);
+				      } else {
+				        $(".number").text(currentInt).toLocaleString('en-US');
+				      }
+			}
+
+			
+		}
+	}
+	
+
+	current = $(".number").text();
+	currentInt = parseInt(current);
+}
+
+function twoththree() {
+
+	var current = $(".number").text();
+	var currentInt = parseInt(current);
+
+
+	countUp();
+
+	function countUp() {
+		var intervv=setInterval(add, 1);
+
+		function add() {
+			if (currentInt > 247950000) {
+				currentInt-=10000;
+				if(currentInt < 247950000) {
+					clearInterval(intervv);
+				} else {
+					$(".number").text(currentInt).toLocaleString('en-US');
+				}
+			} else if(currentInt == 247950000) {
+				clearInterval(intervv);
+			} else {
+				currentInt+=10000;
+				if (currentInt > 247950000) {
+				        clearInterval(intervv);
+				      } else {
+				        $(".number").text(currentInt).toLocaleString('en-US');
+				      }
+			}
+
+			
+		}
+	}
+	
+
+	current = $(".number").text();
+	currentInt = parseInt(current);
+}
+
+function twothfour() {
+
+	var current = $(".number").text();
+	var currentInt = parseInt(current);
+
+
+	countUp();
+
+	function countUp() {
+		var intervv=setInterval(add, 1);
+
+		function add() {
+			if (currentInt > 253640000) {
+				currentInt-=10000;
+				if(currentInt < 253640000) {
+					clearInterval(intervv);
+				} else {
+					$(".number").text(currentInt).toLocaleString('en-US');
+				}
+			} else if(currentInt == 253640000) {
+				clearInterval(intervv);
+			} else {
+				currentInt+=10000;
+				if (currentInt > 253640000) {
+				        clearInterval(intervv);
+				      } else {
+				        $(".number").text(currentInt).toLocaleString('en-US');
+				      }
+			}
+
+			
+		}
+	}
+	
+
+	current = $(".number").text();
+	currentInt = parseInt(current);
+}
+
+function twothfive() {
+
+	var current = $(".number").text();
+	var currentInt = parseInt(current);
+
+
+	countUp();
+
+	function countUp() {
+		var intervv=setInterval(add, 1);
+
+		function add() {
+			if (currentInt > 259380000) {
+				currentInt-=10000;
+				if(currentInt < 259380000) {
+					clearInterval(intervv);
+				} else {
+					$(".number").text(currentInt).toLocaleString('en-US');
+				}
+			} else if(currentInt == 259380000) {
+				clearInterval(intervv);
+			} else {
+				currentInt+=10000;
+				if (currentInt > 259380000) {
+				        clearInterval(intervv);
+				      } else {
+				        $(".number").text(currentInt).toLocaleString('en-US');
+				      }
+			}
+
+			
+		}
+	}
+	
+
+	current = $(".number").text();
+	currentInt = parseInt(current);
+}
+
+
+
+function twothsix() {
+
+	var current = $(".number").text();
+	var currentInt = parseInt(current);
+
+
+	countUp();
+
+	function countUp() {
+		var intervv=setInterval(add, 1);
+
+		function add() {
+			if (currentInt > 265380000) {
+				currentInt-=10000;
+				if(currentInt < 265380000) {
+					clearInterval(intervv);
+				} else {
+					$(".number").text(currentInt).toLocaleString('en-US');
+				}
+			} else if(currentInt == 265380000) {
+				clearInterval(intervv);
+			} else {
+				currentInt+=10000;
+				if (currentInt > 265380000) {
+				        clearInterval(intervv);
+				      } else {
+				        $(".number").text(currentInt).toLocaleString('en-US');
+				      }
+			}
+
+			
+		}
+	}
+	
+
+	current = $(".number").text();
+	currentInt = parseInt(current);
+}
+
+
+
+function twothseven() {
+
+	var current = $(".number").text();
+	var currentInt = parseInt(current);
+
+
+	countUp();
+
+	function countUp() {
+		var intervv=setInterval(add, 1);
+
+		function add() {
+			if (currentInt > 271510000) {
+				currentInt-=10000;
+				if(currentInt < 271510000) {
+					clearInterval(intervv);
+				} else {
+					$(".number").text(currentInt).toLocaleString('en-US');
+				}
+			} else if(currentInt == 271510000) {
+				clearInterval(intervv);
+			} else {
+				currentInt+=10000;
+				if (currentInt > 271510000) {
+				        clearInterval(intervv);
+				      } else {
+				        $(".number").text(currentInt).toLocaleString('en-US');
+				      }
+			}
+
+			
+		}
+	}
+	
+
+	current = $(".number").text();
+	currentInt = parseInt(current);
+}
+
+
+
+function twotheight() {
+
+	var current = $(".number").text();
+	var currentInt = parseInt(current);
+
+
+	countUp();
+
+	function countUp() {
+		var intervv=setInterval(add, 1);
+
+		function add() {
+			if (currentInt > 280360000) {
+				currentInt-=10000;
+				if(currentInt < 280360000) {
+					clearInterval(intervv);
+				} else {
+					$(".number").text(currentInt).toLocaleString('en-US');
+				}
+			} else if(currentInt == 280360000) {
+				clearInterval(intervv);
+			} else {
+				currentInt+=10000;
+				if (currentInt > 280360000) {
+				        clearInterval(intervv);
+				      } else {
+				        $(".number").text(currentInt).toLocaleString('en-US');
+				      }
+			}
+
+			
+		}
+	}
+	
+
+	current = $(".number").text();
+	currentInt = parseInt(current);
+}
+
+
+
+function twothnine() {
+
+	var current = $(".number").text();
+	var currentInt = parseInt(current);
+
+
+	countUp();
+
+	function countUp() {
+		var intervv=setInterval(add, 1);
+
+		function add() {
+			if (currentInt > 285620000) {
+				currentInt-=10000;
+				if(currentInt < 285620000) {
+					clearInterval(intervv);
+				} else {
+					$(".number").text(currentInt).toLocaleString('en-US');
+				}
+			} else if(currentInt == 285620000) {
+				clearInterval(intervv);
+			} else {
+				currentInt+=10000;
+				if (currentInt > 285620000) {
+				        clearInterval(intervv);
+				      } else {
+				        $(".number").text(currentInt).toLocaleString('en-US');
+				      }
+			}
+
+			
+		}
+	}
+	
+
+	current = $(".number").text();
+	currentInt = parseInt(current);
+}
+
+
+
+function twothten() {
+
+	var current = $(".number").text();
+	var currentInt = parseInt(current);
+
+
+	countUp();
+
+	function countUp() {
+		var intervv=setInterval(add, 1);
+
+		function add() {
+			if (currentInt > 293480000) {
+				currentInt-=10000;
+				if(currentInt < 293480000) {
+					clearInterval(intervv);
+				} else {
+					$(".number").text(currentInt).toLocaleString('en-US');
+				}
+			} else if(currentInt == 293480000) {
+				clearInterval(intervv);
+			} else {
+				currentInt+=10000;
+				if (currentInt > 293480000) {
+				        clearInterval(intervv);
+				      } else {
+				        $(".number").text(currentInt).toLocaleString('en-US');
+				      }
+			}
+
+			
+		}
+	}
+	
+
+	current = $(".number").text();
+	currentInt = parseInt(current);
+}
+
+
+
+function twotheleven() {
+
+	var current = $(".number").text();
+	var currentInt = parseInt(current);
+
+
+	countUp();
+
+	function countUp() {
+		var intervv=setInterval(add, 1);
+
+		function add() {
+			if (currentInt > 298080000) {
+				currentInt-=10000;
+				if(currentInt < 298080000) {
+					clearInterval(intervv);
+				} else {
+					$(".number").text(currentInt).toLocaleString('en-US');
+				}
+			} else if(currentInt == 298080000) {
+				clearInterval(intervv);
+			} else {
+				currentInt+=10000;
+				if (currentInt > 298080000) {
+				        clearInterval(intervv);
+				      } else {
+				        $(".number").text(currentInt).toLocaleString('en-US');
+				      }
+			}
+
+			
+		}
+	}
+	
+
+	current = $(".number").text();
+	currentInt = parseInt(current);
+}
+
+
+
+function twothtwelve() {
+
+	var current = $(".number").text();
+	var currentInt = parseInt(current);
+
+
+	countUp();
+
+	function countUp() {
+		var intervv=setInterval(add, 1);
+
+		function add() {
+			if (currentInt > 306160000) {
+				currentInt-=10000;
+				if(currentInt < 306160000) {
+					clearInterval(intervv);
+				} else {
+					$(".number").text(currentInt).toLocaleString('en-US');
+				}
+			} else if(currentInt == 306160000) {
+				clearInterval(intervv);
+			} else {
+				currentInt+=10000;
+				if (currentInt > 306160000) {
+				        clearInterval(intervv);
+				      } else {
+				        $(".number").text(currentInt).toLocaleString('en-US');
+				      }
+			}
+
+			
+		}
+	}
+	
+
+	current = $(".number").text();
+	currentInt = parseInt(current);
+}
+
+
+
+function twoththirteen() {
+
+	var current = $(".number").text();
+	var currentInt = parseInt(current);
+
+
+	countUp();
+
+	function countUp() {
+		var intervv=setInterval(add, 1);
+
+		function add() {
+			if (currentInt > 313050000) {
+				currentInt-=10000;
+				if(currentInt < 313050000) {
+					clearInterval(intervv);
+				} else {
+					$(".number").text(currentInt).toLocaleString('en-US');
+				}
+			} else if(currentInt == 313050000) {
+				clearInterval(intervv);
+			} else {
+				currentInt+=10000;
+				if (currentInt > 313050000) {
+				        clearInterval(intervv);
+				      } else {
+				        $(".number").text(currentInt).toLocaleString('en-US');
+				      }
+			}
+
+			
+		}
+	}
+	
+
+	current = $(".number").text();
+	currentInt = parseInt(current);
+}
+
+
+
+function twothfourteen() {
+
+	var current = $(".number").text();
+	var currentInt = parseInt(current);
+
+
+	countUp();
+
+	function countUp() {
+		var intervv=setInterval(add, 1);
+
+		function add() {
+			if (currentInt > 318620000) {
+				currentInt-=10000;
+				if(currentInt < 318620000) {
+					clearInterval(intervv);
+				} else {
+					$(".number").text(currentInt).toLocaleString('en-US');
+				}
+			} else if(currentInt == 318620000) {
+				clearInterval(intervv);
+			} else {
+				currentInt+=10000;
+				if (currentInt > 318620000) {
+				        clearInterval(intervv);
+				      } else {
+				        $(".number").text(currentInt).toLocaleString('en-US');
+				      }
+			}
+
+			
+		}
+	}
+	
+
+	current = $(".number").text();
+	currentInt = parseInt(current);
+}
+
+
+
+function twothfifteen() {
+
+	var current = $(".number").text();
+	var currentInt = parseInt(current);
+
+
+	countUp();
+
+	function countUp() {
+		var intervv=setInterval(add, 1);
+
+		function add() {
+			if (currentInt > 324240000) {
+				currentInt-=10000;
+				if(currentInt < 324240000) {
+					clearInterval(intervv);
+				} else {
+					$(".number").text(currentInt).toLocaleString('en-US');
+				}
+			} else if(currentInt == 324240000) {
+				clearInterval(intervv);
+			} else {
+				currentInt+=10000;
+				if (currentInt > 324240000) {
+				        clearInterval(intervv);
+				      } else {
+				        $(".number").text(currentInt).toLocaleString('en-US');
+				      }
+			}
+
+			
+		}
+	}
+	
+
+	current = $(".number").text();
+	currentInt = parseInt(current);
+}
+
+
+
+function twothsixteen() {
+
+	var current = $(".number").text();
+	var currentInt = parseInt(current);
+
+
+	countUp();
+
+	function countUp() {
+		var intervv=setInterval(add, 1);
+
+		function add() {
+			if (currentInt > 328100000) {
+				currentInt-=10000;
+				if(currentInt < 328100000) {
+					clearInterval(intervv);
+				} else {
+					$(".number").text(currentInt).toLocaleString('en-US');
+				}
+			} else if(currentInt == 328100000) {
+				clearInterval(intervv);
+			} else {
+				currentInt+=10000;
+				if (currentInt > 328100000) {
+				        clearInterval(intervv);
+				      } else {
+				        $(".number").text(currentInt).toLocaleString('en-US');
+				      }
+			}
+
+			
+		}
+	}
+	
+
+	current = $(".number").text();
+	currentInt = parseInt(current);
+}
+
+
+
+function twothseventeen() {
+
+	var current = $(".number").text();
+	var currentInt = parseInt(current);
+
+
+	countUp();
+
+	function countUp() {
+		var intervv=setInterval(add, 1);
+
+		function add() {
+			if (currentInt > 333590000) {
+				currentInt-=10000;
+				if(currentInt < 333590000) {
+					clearInterval(intervv);
+				} else {
+					$(".number").text(currentInt).toLocaleString('en-US');
+				}
+			} else if(currentInt == 333590000) {
+				clearInterval(intervv);
+			} else {
+				currentInt+=10000;
+				if (currentInt > 333590000) {
+				        clearInterval(intervv);
+				      } else {
+				        $(".number").text(currentInt).toLocaleString('en-US');
+				      }
+			}
+
+			
+		}
+	}
+	
+
+	current = $(".number").text();
+	currentInt = parseInt(current);
+}
+
+
+
+function twotheighteen() {
+
+	var current = $(".number").text();
+	var currentInt = parseInt(current);
+
+
+	countUp();
+
+	function countUp() {
+		var intervv=setInterval(add, 1);
+
+		function add() {
+			if (currentInt > 341160000) {
+				currentInt-=10000;
+				if(currentInt < 341160000) {
+					clearInterval(intervv);
+				} else {
+					$(".number").text(currentInt).toLocaleString('en-US');
+				}
+			} else if(currentInt == 341160000) {
+				clearInterval(intervv);
+			} else {
+				currentInt+=10000;
+				if (currentInt > 341160000) {
+				        clearInterval(intervv);
+				      } else {
+				        $(".number").text(currentInt).toLocaleString('en-US');
+				      }
+			}
+
+			
+		}
+	}
+	
+
+	current = $(".number").text();
+	currentInt = parseInt(current);
+}
+
+
+// $(function() {
+// 		  var cnt0 = 200000000;
+
+// 		  counterFn();
+
+// 		  function counterFn() {
+
+// 		    id0 = setInterval(count0Fn, 100);
+
+// 		    function count0Fn() {
+// 		      cnt0++;
+// 		      if (cnt0 > 99) {
+// 		        clearInterval(id0);
+// 		      } else {
+// 		        $(".number").text(cnt0);
+// 		      }
+// 		    }
+// 		  }
+// 		});
+
+
+
+var ctx3 = document.getElementById("thirdChart").getContext("2d");
+
+var thirdChart = new Chart(ctx3, {
+	type: 'pie',
+	data: {
+		labels: [
+			'Carbon Dioxide',
+			'Methane',
+			'Nitrous Oxide',
+			'HFC, PFC, SF6'
+		],
+		datasets: [{
+			label: 'Percentage of GHG',
+			data: [76, 16, 6, 2],
+			backgroundColor: [
+			'#01118a',
+			'#1225b5',
+			'#3549de',
+			'#4c60fc'
+			],
+			borderColor: [
+			'#01118a',
+			'#1225b5',
+			'#3549de',
+			'#4c60fc'],
+			hoverOffset: 4,
+			spacing: 2
+		}]
+	},
+	options: {
+		plugins: {
+			title: {
+				display: true,
+				text: 'Global Manmade GreenHouse Gas Emissions %',
+				color: 'white',
+				font: {
+					family: "'DM Serif Display', serif",
+					size: 24
+				}
+			},
+			legend: {
+				title: {
+					display: true,
+					padding: 30
+				},
+				labels: {
+					color: 'white',
+					font: {
+						family: "'Cormorant Garamond', serif",
+						size: 14,
+						lineHeight: 5
+					},
+					padding: 30
+				}
+			}
+		}
+	}
+});
+
+var ctx2 = document.getElementById("secondChart").getContext("2d");
+
+var secondChart = new Chart(ctx2, {
+	type: 'line',
+	data: {
+		labels: ["2000",
+		"2001",
+		"2002",
+		"2003",
+		"2004",
+		"2005",
+		"2006",
+		"2007",
+		"2008",
+		"2009",
+		"2010",
+		"2011",
+		"2012",
+		"2013",
+		"2014",
+		"2015",
+		"2016",
+		"2017",
+		"2018"
+		],
+		datasets: [{
+			label: 'South Korea Meat Production',
+			data: [1.65, 1.59, 1.66, 1.78, 1.59, 1.64, 1.77, 1.84, 1.87, 1.97, 2.02, 1.81, 2.10, 2.31, 2.33, 2.42, 2.44, 2.41, 2.50],
+			fill: false,
+			borderColor: '#4f5fc9',
+			backgroundColor: '#4f5fc9',
+			borderWidth: 1
+		}]
+	},
+	options: {
+		responsive: true,
+		maintainAspectRatio: true,
+		plugins: {
+			title: {
+				display: true,
+				text: 'South Korea Production From 2000 to 2018',
+				color: 'white',
+				font: {
+					family: "'DM Serif Display', serif",
+					size: 20
+				}
+			},
+			legend: {
+				display: true,
+				position: 'top',
+				labels: {
+					color: 'white',
+					font: {
+						family: "'Cormorant Garamond', serif",
+						size: 14
+					}
+				}
+			}
+		},
+		scales: {
+			y: {
+				ticks: {
+					beginAtZero: true,
+					color: 'white',
+					font: {
+						family: "'Cormorant Garamond', serif",
+						size: 16
+					}
+				},
+				title: {
+					display: true,
+					text: 'Production Amount in Million Tons (mt)',
+					color: 'white',
+					font: {
+						family: "'Cormorant Garamond', serif",
+						size: 18,
+						lineHeight: 4
+					}
+				},
+				min: 0
+			},
+			x: {
+				ticks: {
+					color: 'white',
+					font: {
+						family: "'Cormorant Garamond', serif",
+						size: 16
+					}
+				}
+			}
+		}
+	}
+
+});
+
+
+
+
